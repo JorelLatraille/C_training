5da02e202723004e33a151161f9e919f6ef22816
JorelLatraille <jlatraille@googlemail.com>
2015-10-22 16:24:39 +0100

    Working copy of tic tac toe.

--- tic_tac_toe/ex1.c
+++ tic_tac_toe/ex1.c
@@ -13,6 +13,7 @@
 #define PLAYER_TWO_VAL 4
 #define PLAYER_ONE_WIN PLAYER_ONE_VAL * NUM_ROWS
 #define PLAYER_TWO_WIN PLAYER_TWO_VAL * NUM_ROWS
+#define DRAW 1
 
 int** createBoard() {
     // allocate Rows rows, each row is a pointer to int
@@ -84,12 +85,28 @@ void printBoard(int **board) {
     }
 }
 
-int* getPlayerMove(char *pName){
-    int finalMove[2];
+int evalMove(int **board, char *player, int *move){
+    int playerVal = 0;
+    if(player == PLAYER_ONE){
+        playerVal = PLAYER_ONE_VAL;
+    }
+    if(player == PLAYER_TWO){
+        playerVal = PLAYER_TWO_VAL;
+    }
+    // check if it is a valid move
+    if(board[move[0]][move[1]] != 0){
+        return 0;
+    }
+    board[move[0]][move[1]] = playerVal;
+    printBoard(board);
+    return 1;
+}
+
+void getPlayerMove(int **board, char *player, int *finalMove){
     bool valid = false;
     while(valid != true){
         char move[MAX_MOVE_LEN] = "   ";
-        printf("Player %s please enter your move e.g. 0,1: ", pName);
+        printf("Player %s please enter your move e.g. 0,1: ", player);
         fgets(move, MAX_MOVE_LEN+1, stdin);
         char *posChars;
         finalMove[0] = (int)strtol(move, &posChars, 10);
@@ -102,22 +119,68 @@ int* getPlayerMove(char *pName){
             printf("The position does not exist please try again.\n");
             continue;
         }
+        if (evalMove(board, player, finalMove) == 0){
+            printf("The position is already taken please try again.\n");
+            continue;
+        }
         printf("%d,%d\n", finalMove[0], finalMove[1]);
         valid = true;
     }
-    return finalMove;
 }
 
-int evalMove(int **board, char *player, int *move){
-    int playerVal = 0;
-    if(player == PLAYER_ONE){
-        playerVal = PLAYER_ONE_VAL;
+int checkWin(int val){
+    switch(val){
+        case PLAYER_ONE_WIN:
+            return PLAYER_ONE_VAL;
+        case PLAYER_TWO_WIN:
+            return PLAYER_TWO_VAL;
     }
-    if(player == PLAYER_TWO){
-        playerVal = PLAYER_TWO_VAL;
+    return 0;
+}
+
+int evalBoard(int **board){
+    int sumRow;
+    int sumCol;
+    int sumHoriz1 = 0;
+    int usedPositions = 0;
+    int result;
+    for(int row=0; row<NUM_ROWS; row++) {
+        sumRow = 0;
+        sumCol = 0;
+        for (int col=0; col<NUM_COLS; col++) {
+            sumRow += (int)board[row][col];
+            sumCol += (int)board[col][row];
+            if (row == col){
+                sumHoriz1 += (int)board[row][col];
+            }
+            if (board[row][col] != 0){
+                usedPositions += 1;
+            }
+
+        result = checkWin(sumRow);
+        if (result){
+            return result;
+        }
+        result = checkWin(sumCol);
+        if (result){
+            return result;
+        }
+        }
+    result = checkWin(sumHoriz1);
+    if (result){
+        return result;
     }
-    board[move[0]][move[1]] = playerVal;
-    printBoard(board);
+    }
+    int sumHoriz2 = (int)board[0][NUM_COLS-1] + (int)board[NUM_ROWS/2][NUM_COLS/2] + (int)board[NUM_ROWS-1][0];
+    switch(sumHoriz2){
+        case PLAYER_ONE_WIN:
+            return PLAYER_ONE_VAL;
+        case PLAYER_TWO_WIN:
+            return PLAYER_TWO_VAL;
+    }
+    if (usedPositions == (NUM_ROWS * NUM_COLS)){
+        return DRAW;
+    }
     return 0;
 }
 
@@ -127,14 +190,24 @@ int main() {
     printBoard(board);
     char *currentPlayer = PLAYER_ONE;
     while(matchResult == 0){
-        int *move = getPlayerMove(currentPlayer);
-        matchResult = evalMove(board, currentPlayer, move);
+        int move[2];
+        getPlayerMove(board, currentPlayer, move);
+        matchResult = evalBoard(board);
         if (currentPlayer == PLAYER_ONE){
             currentPlayer = PLAYER_TWO;
         }
         else {
             currentPlayer = PLAYER_ONE;
         }
+    }
+    if (matchResult == PLAYER_ONE_VAL){
+        printf("Player %s WON!\n", PLAYER_ONE);
+    }
+    if (matchResult == PLAYER_TWO_VAL){
+        printf("Player %s WON!\n", PLAYER_TWO);
+    }
+    if (matchResult == DRAW){
+        printf("DRAW!\n");
     }
     // clean up the memory
     freeBoard(board);
